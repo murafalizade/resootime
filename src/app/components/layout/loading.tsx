969e301c68a874422ce26a05f932ac006c468677
narminzeynalli <narminzeynalliy@gmail.com>
2023-04-29 14:24:20 +0400

    map loader

--- src/app/components/layout/loading.tsx
+++ src/app/components/layout/loading.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from '@/app/styles/Loading.module.scss';
 
-export const Loading = () => {
+export const Loading = ({className}: any) => {
     return (
-        <div data-testid="loading-test" className={styles.spinner_container}>
+        <div data-testid="loading-test" className={`${styles.spinner_container} ${className}`}>
             <div className={styles.loading_spinner}></div>
         </div>
     );

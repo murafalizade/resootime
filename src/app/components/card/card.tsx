ee369a8be6ce921abc9038b6b197b6b80498283b
narminzeynalli <narminzeynalliy@gmail.com>
2023-06-07 21:30:29 +0400

    home-page

--- src/app/components/card/card.tsx
+++ src/app/components/card/card.tsx
@@ -10,49 +10,53 @@ interface ICardProps {
 
 const Card = ({ cardInfo }: ICardProps) => {
     return (
-        <div className={`card ${styles.card}`}>
-            <a
-                href={`/restaurants/r/${cardInfo.name
-                    .replace(' ', '-')
-                    .replace('(', '')
-                    .replace(')', '')
-                    .toLocaleLowerCase()}`}>
-                <Image
-                    src={cardInfo.images[0]?.image || '/images/rest_imag.png'}
-                    className={`card-img-top ${styles.card_img}`}
-                    alt={cardInfo.name}
-                    width={200}
-                    quality={100}
-                    height={140}
-                />
-            </a>
-            <div className="card-body">
+        <div
+            className={`d-flex align-items-center justify-content-center ${styles.card_container}`}>
+            <div className={`card ${styles.card}`}>
                 <a
                     href={`/restaurants/r/${cardInfo.name
                         .replace(' ', '-')
                         .replace('(', '')
                         .replace(')', '')
-                        .toLocaleLowerCase()}`}
-                    className="text-decoration-none text-dark link">
-                    <h5 className="card-title">{cardInfo.name}</h5>
+                        .toLocaleLowerCase()}`}>
+                    <Image
+                        // src={cardInfo.images[0]?.image || '/images/rest_imag.png'}
+                        src={'/images/rest_imag.png'}
+                        className={`card-img-top ${styles.card_img}`}
+                        alt={cardInfo.name}
+                        width={186}
+                        quality={100}
+                        height={96}
+                    />
                 </a>
-
-                <h6 className="card-subtitle mb-2">
-                    <div className="d-flex">
-                        <span className="d-flex align-items-center">
-                            <StarsRating
-                                count={1}
-                                style={{ style: { fontSize: '20px' } }}
-                                disabled
-                                value={1}
-                            />
-                            {cardInfo.rate ?? 0}
+                <div className={`card-body px-0 ${styles.card_body}`}>
+                    <span className={`${styles.res_type}`}>Restoran</span>
+                    <div className="d-flex justify-content-between">
+                        <a
+                            href={`/restaurants/r/${cardInfo.name
+                                .replace(' ', '-')
+                                .replace('(', '')
+                                .replace(')', '')
+                                .toLocaleLowerCase()}`}
+                            className="text-decoration-none text-dark link">
+                            <h5 className="card-title">ResooTime (Test)</h5>
+                        </a>
+                        <span className={`m-0  ${styles.res_rate}`}>
+                            {`${cardInfo.rate ?? 0}.0`}
                         </span>
-                        <p className="text-muted align-items-center d-flex mx-3 my-0 p-0">
-                            {cardInfo?.city}
-                        </p>
                     </div>
-                </h6>
+                    <h6 className="card-subtitle m-0">
+                        <div className="d-flex">
+                            <p className={`m-0 ${styles.res_location}`}>
+                                {cardInfo.city || 'World'}
+                            </p>
+                        </div>
+                    </h6>
+                    <div className={`d-flex`}>
+                        <div className={`${styles.res_tag}`}>Musiqili</div>
+                        <div className={`${styles.res_tag}`}>Mənzrəli</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
